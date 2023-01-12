96cbe86464cf28274e05b88b93cae0281efea0cf
Martín Caminero <mcaminero@intermediait.com>
2023-01-12 10:05:12 -0300

    adding string resource to avoide using hardcoded strings on the app

--- app/src/main/java/com/example/weatherapp/presentation/splashScreen/SplashScreen.kt
+++ app/src/main/java/com/example/weatherapp/presentation/splashScreen/SplashScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,7 +55,7 @@ fun SplashAnimation(isVisible: Boolean) {
             )
             Image(
                 painter = painterResource(id = R.drawable.logo),
-                contentDescription = "Weather app logo"
+                contentDescription = stringResource(R.string.content_description_for_splash_logo)
             )
         }
     }

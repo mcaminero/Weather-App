ee8ea10b47543d76ee4831fcbe80156f4a22d62b
Martín Caminero <mcaminero@intermediait.com>
2023-01-19 14:05:11 -0300

    somehow this are deleted files, but they still exist in the project

--- app/src/main/java/com/example/weatherapp/presentation/Screens.kt
+++
@@ -1,6 +0,0 @@
-package com.example.weatherapp.presentation
-
-sealed class Screens(val route: String){
-    object SplashScreen: Screens("splash_screen")
-    object MainScreen: Screens("main_screen")
-}

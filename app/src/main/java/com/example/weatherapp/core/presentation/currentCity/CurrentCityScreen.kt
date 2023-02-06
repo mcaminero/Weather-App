64da4ed532842a0a61490e044efb9b4ccd44a4c8
Martín Caminero <mcaminero@intermediait.com>
2023-02-06 11:32:57 -0300

    Update visual and permission request
    In MainActivity I added the annotation AndroidEntryPoint, I was asking
    for location persmission but those request were move to
    CurrentCityScreen
    
    In CurrentCityScreen I made the request to check for location permission
    I also moved the getWeather method inside the if statement to avoid
    getting a null location provider
    
    In the SplashScreen I only added a fadeIn enter animation
    
    In Navigation there was only a imports clean up, I was having to many
    imports that I was not using

--- app/src/main/java/com/example/weatherapp/core/presentation/currentCity/CurrentCityScreen.kt
+++ app/src/main/java/com/example/weatherapp/core/presentation/currentCity/CurrentCityScreen.kt
@@ -2,28 +2,24 @@ package com.example.weatherapp.core.presentation.currentCity
 
 import android.Manifest
 import android.app.Activity
-import android.util.Log
 import androidx.activity.compose.BackHandler
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.weatherapp.core.presentation.navigation.MainNavigationGraph
 import com.example.weatherapp.utils.BottomNavItems
-import com.example.weatherapp.utils.Screens
 import com.example.weatherapp.utils.items
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
@@ -33,20 +29,11 @@ import com.google.accompanist.permissions.rememberMultiplePermissionsState
 fun MainScreen(navController: NavHostController = rememberNavController()) {
     val backStackEntry = navController.currentBackStackEntryAsState()
     val currentRoute = backStackEntry.value?.destination?.route
-    val isSplashScreen = currentRoute == Screens.SplashScreen.route
+    //val isSplashScreen = currentRoute == Screens.SplashScreen.route
+
     Scaffold(bottomBar = {
-        if (!isSplashScreen)
-            BottomNavigationBar(
-                items = items, currentRoute = currentRoute
-            ){
-                navController.navigate(it){
-                    popUpTo(navController.graph.findStartDestination().id){
-                        saveState = true
-                    }
-                    launchSingleTop = true
-                    restoreState = true
-                }
-            }
+        /*if (!isSplashScreen) */
+        BottomNavigationBar(navController)
     }) {
         MainNavigationGraph(navController = navController, modifier = Modifier.padding(it))
     }
@@ -54,69 +41,98 @@ fun MainScreen(navController: NavHostController = rememberNavController()) {
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun CurrentCityScreen(navController: NavHostController) {
+fun CurrentCityScreen(
+    navController: NavHostController,
+    viewModel: CurrentCityViewModel = hiltViewModel()
+) {
     val activity = LocalContext.current as Activity
-    val permissions = rememberMultiplePermissionsState(
+    val currentCity by viewModel.currentCityUiState.collectAsState()
+    val permissionsState = rememberMultiplePermissionsState(
         permissions = listOf(
-            Manifest.permission.ACCESS_COARSE_LOCATION,
-            Manifest.permission.ACCESS_FINE_LOCATION
+            Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION
         )
-    )
-    rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission())
-    { isGranted ->
-        if (isGranted) {
-            // Permission Accepted: Do something
-            Log.d("ExampleScreen", "PERMISSION GRANTED")
-
-        } else {
-            // Permission Denied: Do something
-            Log.d("ExampleScreen", "PERMISSION DENIED")
+    ) { permissions ->
+        if (permissions.values.any { !it }){
+            activity.finishAffinity()
         }
-    }.apply {
-        if (!permissions.allPermissionsGranted) {
-            LaunchedEffect(key1 = true) {
-                launch(Manifest.permission.ACCESS_COARSE_LOCATION)
-                launch(Manifest.permission.ACCESS_FINE_LOCATION)
+    }
+    if (permissionsState.allPermissionsGranted) {
+            viewModel.getWeather()
+        currentCity.let { cityUiState ->
+            Box(modifier = Modifier.fillMaxSize()) {
+                if (cityUiState.isLoading) {
+                    CircularProgressIndicator(
+                        strokeWidth = 4.dp,
+                        modifier = Modifier
+                            .align(Alignment.Center)
+                            .padding(16.dp)
+                    )
+                } else {
+                    LazyColumn(modifier = Modifier.fillMaxWidth()) {
+                        items(cityUiState.currentCityData?.hourly!!.windSpeed) {
+                            Text(
+                                text = "wind speed: $it",
+                                modifier = Modifier.align(Alignment.Center)
+                            )
+                        }
+                    }
+
+                }
             }
-
+        }
+    } else {
+        LaunchedEffect(key1 = true) {
+            permissionsState.launchMultiplePermissionRequest()
         }
     }
-
-    Box(modifier = Modifier.fillMaxSize()) {
-        Text(
-            text = "Current City Screen",
-            modifier = Modifier.align(Alignment.Center)
-        )
-    }
-
     BackHandler {
-        activity.finishAndRemoveTask()
+        activity.finishAffinity()
     }
 }
 
 @Composable
-fun BottomNavigationBar(items: List<BottomNavItems>,currentRoute: String?, onItemClick: (String) -> Unit) {
+fun BottomNavigationBar(navController: NavHostController) {
+    val backStackEntry = navController.currentBackStackEntryAsState()
+    val currentRoute = backStackEntry.value?.destination?.route
+
+    BottomNavigationBar(
+        items = items,
+        currentRoute = currentRoute,
+        onItemClick = {
+            navController.navigate(it) {
+                popUpTo(navController.graph.findStartDestination().id) {
+                    saveState = true
+                }
+                launchSingleTop = true
+                restoreState = true
+            }
+        }
+    )
+}
+
+@Composable
+fun BottomNavigationBar(
+    items: List<BottomNavItems>,
+    currentRoute: String?,
+    onItemClick: (String) -> Unit
+) {
     BottomNavigation {
         items.forEach { item ->
-            BottomNavigationItem(
-                selected = currentRoute == item.route,
-                onClick = {
-                    onItemClick(item.route)
-                },
-                icon = {
-                    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-                        Icon(item.icon, contentDescription = "")
-                        Text(text = item.name)
-                    }
+            BottomNavigationItem(selected = currentRoute == item.route, onClick = {
+                onItemClick(item.route)
+            }, icon = {
+                Column(horizontalAlignment = Alignment.CenterHorizontally) {
+                    Icon(item.icon, contentDescription = "")
+                    Text(text = item.name)
                 }
+            }
             )
         }
     }
 }
 
-
 @Preview()
 @Composable
 fun BottomNavigationBarPrev() {
-    BottomNavigationBar(items,null) {}
+    BottomNavigationBar(rememberNavController())
 }

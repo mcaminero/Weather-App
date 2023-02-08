8c4be94c506d0d8c8c95af7e09c352cd90612c81
Martín Caminero <mcaminero@intermediait.com>
2023-02-08 11:54:41 -0300

    untrack file and rename classes from project

--- app/src/main/java/com/example/weatherapp/core/data/dto/CurrentWeather.kt
+++
@@ -1,9 +0,0 @@
-package com.example.weatherapp.core.data.dto
-
-data class CurrentWeather(
-    val temperature: Double,
-    val time: String,
-    val weathercode: Int,
-    val winddirection: Double,
-    val windspeed: Double
-)
